5f3492d1497beec7cfbcbddbde07a4a8b07de151
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-10-28 10:26:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//console.log("oi")
+
+//---------- FOR ----------
+
+//for(let i = 1; i <= 5; i++){
+//    console.log("Bom dia!")
+//}
+
+//for(let i = 2; i <= 10; i += 2){
+//    console.log(i)
+//}
+
+//---------- WHILE ----------
+
+//let iphone = 1
+
+//while(iphone <= 16){
+//    console.log(iphone)
+//    iphone++
+//}
+
+//let i = 5
+//while(i > 0){
+//    console.log(i)
+//    i--
+//}
+
+//---------- OPERADORES LÓGICOS ----------
+
+//&& <--- e/and
+//|| <--- ou/or
+
+let login = "vini"
+let senha = 1234
+
+if(login == "vini" && senha == 1234){
+    console.log("Acesso liberado")
+} else{
+    console.log("Negado")
+}
+
+//---------- OPERADORES DE OPERAÇÃO ----------
+
+//+ <--- soma
+console.log(2+3)
+
+//- <--- subtração
+console.log(3-2)
+
+//* <--- multiplicação
+console.log(3*2)
+
+// / <--- divisão
+console.log(3/2)
+
+//++ <--- incremento
+let i = 4
+console.log(++i)
